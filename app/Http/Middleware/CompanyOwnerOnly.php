0085043c772c8de26661a4e869380c8616a9dc6d
rdb2898 <rdc2898@gmail.com>
2018-03-07 19:31:30 +0800

    middleware bug fix

--- app/Http/Middleware/CompanyOwnerOnly.php
+++ app/Http/Middleware/CompanyOwnerOnly.php
@@ -15,7 +15,7 @@ class CompanyOwnerOnly
      */
     public function handle($request, Closure $next)
     {
-      if (Auth::check() && Auth::user()->role == 2)
+      if (Auth::check() && Auth::user()->role == 1)
       {
         return $next($request);
       }else {
